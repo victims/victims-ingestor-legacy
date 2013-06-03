f5d0b7c414c08fd14b77fe58a139adec5845e083
Pramod Dematagoda <pmd.lotr.gandalf@gmail.com>
2013-06-03 19:02:43 +1000

    Remove hashing code in victim_file.py as it is not needed since it is done by the web client

--- src/victim_file.py
+++ src/victim_file.py
@@ -19,8 +19,7 @@ http://rubygems.org/downloads/<package-name>-<package-version>.gem - ruby downlo
 https://pypi.python.org/packages/source/<first-letter-of-package-name>/<package-name>/<package-name>-<package-version>.tar.gz - python download url template
 
 '''
-
-def  download_file (url):
+def download_file (url):
     ''' 
     function to try and download the file in the given url
     '''
@@ -52,7 +51,7 @@ def _make_package_url (package_name, package_version, lang):
     else:
         return python_template.format (package_name[0], package_name, package_name, package_version)
 
-def _get_package (package_name, package_version, lang):
+def package_exists (package_name, package_version, lang):
     '''
     a function linking the required functions to download a required package
     '''
@@ -60,48 +59,7 @@ def _get_package (package_name, package_version, lang):
     url = _make_package_url (package_name, package_version, lang)
     victim_file = download_file (url)
 
-    return victim_file
-
-''' prospective function to be implemented that hashes the given files '''
-def _hash_file (victims_file):
-    return 0
-
-class VictimsFile:
-    '''
-    class that represents a vulnerable package
-    '''
-
-    file_name = None
-    download_file = None
-    cve_id = ""
-    vendor = ""
-    version = ""
-    name = ""
-
-    def __init__ (self, package_name, package_version, vendor_name=None, cve=None, lang=None):
-
-        if lang is None: # if no language is specified, try and find out for ourselves
-            ret = _get_package (package_name, package_version, 0)
-
-            if ret is None:
-                ret = _get_package (package_name, package_version, 1)
-
-                if ret is None:
-                    return
-
-        else:
-            ret = _get_package (package_name, package_version, lang)
-            if ret is None:
-                return
-
-        self.download_file = ret
-        self.cve_id = cve
-        self.vendor = vendor_name
-        self.name = package_name
-        self.version = package_version
-
-    def get_file (self):
-        return self.download_file
-
-    def get_file_name (self):
-        return self.file_name
+    if victim_file is None:
+        return False
+    else:
+        return True
