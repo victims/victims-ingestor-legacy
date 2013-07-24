28f09962b944fd0d93ff919ff0d8bc3ba7c841a7
Pramod Dematagoda <pmd.lotr.gandalf@gmail.com>
2013-07-24 21:21:19 +1000

    PEP8 fixes and use environment variables for DB init

--- src/victim_db_manager.py
+++ src/victim_db_manager.py
@@ -9,7 +9,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 '''
-Module that provides a class that helps working with the victims hash collection
+Module that provides a class that helps working with the
+victims hash collection
 '''
 
 import pymongo
@@ -22,49 +23,77 @@ class VictimDB:
 
     __hash_table = None
 
-    def __init__ (self, db_name='victims', host='localhost', port=27017, table='hashes'):
+    def __init__ (self, db_name='victims',
+                  host=os.getenv ("OPENSHIFT_MONGODB_DB_HOST"),
+                  port=int (os.getenv ("OPENSHIFT_MONGODB_DB_PORT")),
+                  table='hashes'):
 
         try:
-            # If pymongo is version 2.3 or less we need to get a Connection object
+            '''
+            If pymongo is version 2.3 or less we need
+            to get a Connection object
+            '''
             if float (pymongo.version) <= 2.3:
                 client = pymongo.Connection (host, port)
             else:
                 client = pymongo.MongoClient (host, port)
 
         except ConnectionFailure:
-            raise ConnectionFailure ()
+            raise
 
         try:
             db = client[db_name]
+
+            if (os.getenv ("OPENSHIFT_MONGODB_DB_USERNAME") and
+                os.getenv ("OPENSHIFT_MONGODB_DB_PASSWORD")):
+                '''
+                Authenticate against the given database
+                using the credentials in the environment
+                '''
+                db.authenticate (os.getenv ("OPENSHIFT_MONGODB_DB_USERNAME"),
+                                 os.getenv ("OPENSHIFT_MONGODB_DB_PASSWORD"))
 
         except InvalidName:
             raise ConnectionFailure ()
 
         self.__hash_table = pymongo.collection.Collection (db, table)
-            
 
-    def add_victim (self, cve_list, vendor, package_name, package_version, package_format, state='PENDING'):
+
+    def add_victim (self, cve_list,
+                    vendor,
+                    package_name,
+                    package_version,
+                    package_format,
+                    state='PENDING'):
         """
         Adds a potential victim entry to the victims database
         """
 
-        if self.__hash_table.find ({'name' : package_name, 'version' : package_version}) is not None:
+        if self.__hash_table.find ({'name' : package_name,
+                                    'version' : package_version}) is not None:
             return -1
         else:
-            self.__hash_table.insert ({'name' : package_name, 'version' : package_version, 'vendor' : vendor, 'format' : package_format, 'hash' : hash_id, 'state' : state})
+            self.__hash_table.insert ({'name' : package_name,
+                                       'version' : package_version,
+                                       'vendor' : vendor,
+                                       'format' : package_format,
+                                       'hash' : hash_id,
+                                       'state' : state})
 
         return 0
 
     def get_victim_entry (self, package_name, package_version):
         """
-        Get a single victim entry that corresponds to the given parameters
+        Get a single victim entry that
+        corresponds to the given parameters
         """
 
         return self.__hash_table.findOne ({'name' : package_name, 'version' : package_version})
 
     def get_victim_entries (self, package_name, package_version):
         """
-        Get a list of all victim entries that correspond to the given parameters
+        Get a list of all victim entries that
+        correspond to the given parameters
         """
 
         return self.__hash_table.find ({'name' : package_name, 'version' : package_version})
