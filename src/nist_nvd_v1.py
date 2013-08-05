e560ccd2123764be88fba46a0058a667ecad2eb6
Pramod Dematagoda <pmd.lotr.gandalf@gmail.com>
2013-08-05 23:19:40 +1000

    Caching ability added to NIST v1 source plugin

--- src/nist_nvd_v1.py
+++ src/nist_nvd_v1.py
@@ -10,76 +10,90 @@
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 import urllib2
+from datetime import datetime
 
 import xml.parsers.expat
 
 from victim_file import download_file
+import victim_db_manager
 
 # TODO - cache the results after a run, it currently takes ages for a single run
 # - Implement threading in the library to make runs faster
-# - May be use CVE v1 files as these are much smaller? (Are they good enough?)
 
 # Language agnostic databases
-cve_sources_recent = ["http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-modified.xml",
-                      "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-recent.xml"]
-
-cve_sources_full = ["http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2002.xml",
-                    "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2003.xml",
-                    "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2004.xml",
-                    "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2005.xml",
-                    "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2006.xml",
-                    "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2007.xml",
-                    "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2008.xml",
-                    "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2009.xml",
-                    "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2010.xml",
-                    "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2011.xml",
-                    "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2012.xml",
-                    "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2013.xml"]
+cve_sources_full = ["http://nvd.nist.gov/download/nvdcve-2002.xml",
+                    "http://nvd.nist.gov/download/nvdcve-2003.xml",
+                    "http://nvd.nist.gov/download/nvdcve-2004.xml",
+                    "http://nvd.nist.gov/download/nvdcve-2005.xml",
+                    "http://nvd.nist.gov/download/nvdcve-2006.xml",
+                    "http://nvd.nist.gov/download/nvdcve-2007.xml",
+                    "http://nvd.nist.gov/download/nvdcve-2008.xml",
+                    "http://nvd.nist.gov/download/nvdcve-2009.xml",
+                    "http://nvd.nist.gov/download/nvdcve-2010.xml",
+                    "http://nvd.nist.gov/download/nvdcve-2011.xml",
+                    "http://nvd.nist.gov/download/nvdcve-2012.xml",
+                    "http://nvd.nist.gov/download/nvdcve-2013.xml"]
 
 cve = ""          # The current CVE ID/s being parsed
 p_name = ""         # The current package name being parsed
 valid = False     # Is the entry currently being processed something we want? a global because the value's needed by two functions
 vuln_list = None    # The dictionary of valid vulnerable entries currently parsed
 
-DEBUG_MODE = True # Is debug mode on?
+cache_db = None
 
+DEBUG_MODE = True # Is debug mode on?
+CACHING = True # Is caching turned on?
+
+def _cache_uptodate ():
+    """
+    Check if the cache is up to date
+    """
+    if cache_db.check_mtime_within ():
+        return True
+
+    cache_db.renew_table ()
+
+    return False
 
 def get_entries (output_dict):
-    '''
+    """
     Function parses and aggregates _all_ vulnerability information from the predefind sources
 
     output is a dictionary of the following format:
     dict[package_name] - returns a dictionary(dict2) of the format
     dict2[version] which gives a list of CVEs affecting the given version
     dict2[vendor] returns the vendor for the given package_name
-    '''
+    """
     global vuln_list
 
     vuln_list = output_dict
 
-    for src in cve_sources_recent:
+    if CACHING:
+        global cache_db
+
+        cache_db = victim_db_manager.VictimDB (table="cache_nistv1")
+
+        if _cache_uptodate ():
+            return cache_db.get_cache ()
+
+    for src in cve_sources_full:
         source = _get_source (src)
         if source is None:
             continue
         else:
             _parse_nvd_file (source)
 
-    
-    if not DEBUG_MODE: 
-        for src in cve_sources_full:
-            source = _get_source (src)
-            if source is None:
-                continue
-            else:
-                _parse_nvd_file (source)
+    if CACHING:
+        cache_db.create_cache (vuln_list)
+        cache_db.add_mtime_stamp ()
 
     return vuln_list
 
 
 def _get_source (url):
-    '''
+    """
     Function that tries to download a file from the given url
-    '''
+    """
 
     try:
         ret = urllib2.urlopen (url)
@@ -96,9 +110,9 @@ def _get_source (url):
 
 
 def _parse_helper_nvd (name, attr):
-    '''
+    """
     Function that checks if data currently being processed is what we're looking for
-    '''
+    """
     global valid
     global cve
     global p_name
@@ -123,14 +137,14 @@ def _parse_helper_nvd (name, attr):
 
         if version not in vuln_list[p_name]:
             vuln_list[p_name][version] = [cve]
-        else:            
+        else:
             vuln_list[p_name][version].append (cve)
 
 
 def _parse_nvd_file (input_file):
-    '''
+    """
     Function to parse the data in the nvd file to find the appropriate cve entries and the program name and version
-    '''
+    """
     nvd_parser = xml.parsers.expat.ParserCreate ()
     nvd_parser.StartElementHandler = _parse_helper_nvd
     nvd_parser.ParseFile (input_file)
